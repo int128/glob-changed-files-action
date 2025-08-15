4d9d437cc8e5b59d178d8ebc54f2881c3c8a5d21
Hidetake Iwata <int128@gmail.com>
2025-08-15 15:18:37 +0900

    Support fallback to match-working-directory (#1182)
    * Support fallback to working directory
    
    * doc
    
    * Pin
    
    * Rename

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 import * as match from './match.js'
 import { Context } from './github.js'
 import { Octokit } from '@octokit/action'
@@ -6,6 +7,7 @@ import { Octokit } from '@octokit/action'
 type Inputs = {
   paths: string[]
   pathsFallback: string[]
+  fallbackMethod: 'wildcard' | 'match-working-directory'
   outputsMap: Map<string, string>
   outputsEncoding: 'multiline' | 'json'
 }
@@ -35,15 +37,15 @@ type VariableMap = Map<string, string[]>
 
 const matchChangedFiles = async (inputs: Inputs, context: Context, octokit: Octokit): Promise<VariableMap> => {
   if (!('pull_request' in context.payload && 'number' in context.payload)) {
-    core.info(`Fallback to wildcard because not pull_request event`)
-    return fallbackToWildcard(inputs.outputsMap)
+    core.info(`Fallback due to not pull_request event`)
+    return await fallback(inputs)
   }
 
   // Limit the max number of changed files to prevent GitHub API rate limit
   core.info(`${context.payload.pull_request.changed_files} files are changed in the pull request`)
   if (context.payload.pull_request.changed_files > 1000) {
-    core.info(`Fallback to wildcards due to too many changed files`)
-    return fallbackToWildcard(inputs.outputsMap)
+    core.info(`Fallback due to too many changed files`)
+    return await fallback(inputs)
   }
 
   core.info(`List files in the pull request`)
@@ -61,12 +63,36 @@ const matchChangedFiles = async (inputs: Inputs, context: Context, octokit: Octo
   core.info(`Received a list of ${changedFiles.length} files`)
 
   if (match.matchAny(inputs.pathsFallback, changedFiles)) {
-    core.info(`Fallback to wildcard because paths-fallback matches to the changed files`)
-    return fallbackToWildcard(inputs.outputsMap)
+    core.info(`Fallback due to paths-fallback matches to the changed files`)
+    return await fallback(inputs)
   }
 
   const groups = match.matchGroups(inputs.paths, changedFiles)
   core.info(`Transform paths by the changed files`)
+  const variableMap = new Map<string, string[]>()
+  for (const [key, pattern] of inputs.outputsMap) {
+    const paths = match.transform(pattern, groups)
+    variableMap.set(key, paths)
+  }
+  return variableMap
+}
+
+const fallback = async (inputs: Inputs): Promise<VariableMap> => {
+  if (inputs.fallbackMethod === 'wildcard') {
+    return fallbackToWildcard(inputs.outputsMap)
+  }
+  return await matchWorkingDirectory(inputs)
+}
+
+const matchWorkingDirectory = async (inputs: Inputs): Promise<VariableMap> => {
+  core.startGroup(`git ls-files`)
+  const { stdout } = await exec.getExecOutput('git', ['ls-files'])
+  const workingDirectoryFiles = stdout.trim().split('\n')
+  core.endGroup()
+
+  core.info(`Working directory files: ${workingDirectoryFiles.length} files`)
+  const groups = match.matchGroups(inputs.paths, workingDirectoryFiles)
+  core.info(`Transform paths by the working directory files`)
   const variableMap = new Map<string, string[]>()
   for (const [key, pattern] of inputs.outputsMap) {
     const paths = match.transform(pattern, groups)
