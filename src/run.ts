304f923750714d6f41d178625f7a295bb188beb2
Hidetake Iwata <int128@gmail.com>
2025-08-30 14:45:55 +0900

    Refactor

--- src/run.ts
+++ src/run.ts
@@ -19,9 +19,6 @@ type Outputs = {
 }
 
 export const run = async (inputs: Inputs, context: Context, octokit: Octokit): Promise<Outputs> => {
-  core.info(`eventName: ${context.eventName}`)
-  core.info(`outputs: ${JSON.stringify([...inputs.outputsMap], undefined, 2)}`)
-
   if (!('pull_request' in context.payload && 'number' in context.payload)) {
     core.info(`Fallback due to not pull_request event`)
     return await fallback(inputs)
